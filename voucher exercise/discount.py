9ba5963978ccfee08f575965c53ed670a0c91f86
Ruochen Li <sc16rl@gmail.com>
2019-07-15 09:30:05 +0800

    some modifications

--- voucher exercise/discount.py
+++ voucher exercise/discount.py
@@ -7,7 +7,17 @@ array = list(l +l.upper())
 def shuffle():
     global array
     random.shuffle(array)
-    return ''.join(array[:8])
+    x = ''.join(array[:8])
+    return x
 
-for i in range(200):
-    print(shuffle())
+new_list = []
+i=0
+while i < 200:
+    if shuffle() in new_list:
+        continue
+    else:
+        new_list.append(shuffle())
+        i+=1
+
+for j in new_list:
+    print(j)
